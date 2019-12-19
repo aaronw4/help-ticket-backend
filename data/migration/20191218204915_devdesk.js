d003943f27a32709f10708b4bd37effeec58b7bc
Brandon Nichols <brandonaronnichols@gamil.com>
2019-12-18 21:53:48 -0600

    setup knex schema

--- data/migration/20191218204915_devdesk.js
+++ data/migration/20191218204915_devdesk.js
@@ -1,6 +1,65 @@
 exports.up = function(knex) {
-  //   return knex.schema.createTable('helpers', tbl => {
-  //   })
+  return knex.schema
+    .createTable("helpers", tbl => {
+      tbl.increments();
+      tbl
+        .string("username")
+        .unique()
+        .notNullable();
+      tbl.string("password").notNullable();
+    })
+    .createTable("categories", tbl => {
+      tbl.increments();
+      tbl
+        .string("category")
+        .notNullable()
+        .unique();
+    })
+    .createTable("students", tbl => {
+      tbl.increments();
+      tbl
+        .string("username")
+        .unique()
+        .notNullable();
+      tbl.string("password").notNullable();
+      tbl.string("title").notNullable();
+      tbl.string("description").notNullable();
+      tbl.string("attempted").notNullable();
+      tbl
+        .integer("categoryId")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("categories")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("tickets", tbl => {
+      tbl.increments();
+      tbl
+        .integer("helpId")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("helpers")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("studentId")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("students")
+        .onUpdate("CASCADE")
+        .onUpdate("CASCADE");
+      tbl.primary(["helpId", "studentId"]);
+    });
 };
 
-exports.down = function(knex) {};
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists("tickets")
+    .dropTableIfExists("students")
+    .dropTableIfExists("categories")
+    .dropTableIfExists("helpers");
+};
--- knexfile.js
+++ knexfile.js
@@ -3,13 +3,21 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    client: "pg",
-    connection: process.env.DB_URL,
+    client: "sqlite3",
+    connection: {
+      filename: "./data/dev-desk.db3"
+    },
+    useNullAsDefault: true,
     migrations: {
       directory: "./data/migration"
     },
     seeds: {
       directory: "./data/seed"
+    },
+    pool: {
+      afterCreation: function(connection, done) {
+        connection.run("PRAGMA foreign_keys = ON", done);
+      }
     }
   },
 
