e886842b3b5981248106a3bc1a2c39e0b46e55b0
Brandon Nichols <brandonaronnichols@gamil.com>
2019-12-19 22:16:15 -0600

    added register route to helpers-route.js

--- helpers/helpers-models.js
+++ helpers/helpers-models.js
@@ -1,9 +1,14 @@
 const db = require("../config/dbConfig");
 
 module.exports = {
-  getHelpers
+  getHelpers,
+  addHelper
 };
 
 function getHelpers() {
   return db("helpers");
+}
+
+function addHelper(helper) {
+  return db("helpers").insert(helper);
 }
--- helpers/helpers-route.js
+++ helpers/helpers-route.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 const Helper = require("./helpers-models");
 const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  let { username, password } = req.body;
+  const hash = bcrypt.hash(password, 12);
+  password = hash;
+
+  try {
+    const addedUser = await Helper.addHelper({ username, password });
+    res.status(200).json(addedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 router.get("/", async (req, res) => {
   try {
