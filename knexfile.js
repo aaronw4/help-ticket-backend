87453d8afe18b862cba83a4742728b9dc9045fc3
Brandon Nichols <brandonaronnichols@gamil.com>
2019-12-20 02:09:51 -0600

    changed knexfile back to just postgress and removed some scripts from package.json

--- knexfile.js
+++ knexfile.js
@@ -3,16 +3,8 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./data/devdesk.db3"
-    },
-    useNullAsDefault: true,
-    pool: {
-      afterCreate: function(connection, done) {
-        connection.run("PRAGMA foreign_keys = ON", done);
-      }
-    },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./data/migration"
     },
