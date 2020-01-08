798227d45b391f3bc11ba6347f3e685d5fc1b128
Brandon Nichols <brandonaronnichols@gamil.com>
2020-01-08 15:22:49 -0600

    changed as clause to be called query

--- auth/tickets/tickets-models.js
+++ auth/tickets/tickets-models.js
@@ -65,7 +65,7 @@ function getCategories() {
 function getUsersTickets(userId) {
   return db.raw(
     `select * from users_tickets join 
-    (select tickets.id, users.username, tickets.userId as ticketUserId from tickets join users on users.id = tickets.userId) as ticketQuery
-    on users_tickets.ticketId = ticketQuery.id where userId = ${userId}`
+    (select tickets.id, users.username, tickets.userId as ticketUserId from tickets join users on users.id = tickets.userId) as query
+    on users_tickets.ticketId = query.id where userId = ${userId}`
   );
 }
